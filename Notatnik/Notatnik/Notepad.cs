efcc404ab26f4781635e3d084a965dbcd535e489
AndrzejPliszka <andrzejpliszka3@gmail.com>
2025-11-24 09:26:28 +0100

    Nested list conversion to elements attempt (dont merge this into  main !)

--- Notatnik/Notatnik/Notepad.cs
+++ Notatnik/Notatnik/Notepad.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.Win32;
 using Notatnik.Elements;
+using System;
 using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
@@ -19,10 +20,18 @@ namespace Notatnik
     public partial class Notepad
     {
         //As when I am writing this UI is not implemented, so this var acts like text in TextBox (for OpenFile and SaveFile purposes)!
-        public string writtenText = "# Test of Header\nTest of *paragraph*\nwhich has __two__ lines\n_________________\n1. This is first element of list\n8. that's a __second__ element\n- and this is unordered list\n- which has two elements";
+        public string writtenText = "* This is first element of list\n* this is second element\n    * wow this is nested list\n        * nested lsit in nested list :0\n    * second element of nested list";
 
         public List<Element> content = [];
         public GitHubUser user = new();
+
+        public Notepad()
+        {
+            FormatText();
+            MessageBox.Show(content.Count + "");
+            MessageBox.Show(ParseIntoString());
+            SaveFile();
+        }
 
         //Format text from string form into list of elements
         public void FormatText()
@@ -47,20 +56,55 @@ namespace Notatnik
                 }
 
                 //If is unordered list
-                if(UnorderedListRegex().IsMatch(currentLine))
+                var match = UnorderedListRegex().Match(currentLine);
+                if (match.Success)
                 {
-                    Elements.MarkdownList list;
-                    if(content.Count > 0 && content[^1] is Elements.MarkdownList existingList && !existingList.isOrdered)
+                    string indent = match.Groups["indent"].Value;
+                    int indentAmount = Regex.Matches(indent, @"\t| {4}").Count;
+                    int previousIndentAmount = 0;
+                    MarkdownList? list;
+                    if (content.Count > 0 && content[^1] is Elements.MarkdownList existingList && !existingList.isOrdered)
                     {
-                        list = existingList;
+                        Elements.MarkdownList lastList = existingList;
+                        List<Elements.ListItem> listContent = existingList.content;
+                        MessageBox.Show(listContent.Count + " " + listContent[^1].content.Count + " " + listContent[^1].content[^1].GetType() + " " + listContent[^1].content[^1]);
+                        previousIndentAmount = 1;
+                        while (listContent.Count > 0 && listContent[^1].content.Count > 0 && listContent[^1].content[^1] is Elements.MarkdownList internalList)
+                        {
+                            listContent = internalList.content;
+                            lastList = internalList;
+                            previousIndentAmount = internalList.nestingDepth;
+                        }
+
+                        MessageBox.Show(indentAmount + " " + previousIndentAmount);
+                        if (lastList != null && indentAmount == previousIndentAmount)
+                        {
+                            lastList.AddListElement(SplitStringByStyles(currentLine[(2 + 4 * indentAmount)..]));
+                            continue;
+                        }
+                        else if (indentAmount - 1 == previousIndentAmount)
+                        {
+                            MessageBox.Show("test lest");
+                            Elements.MarkdownList markdownList = new(true);
+                            markdownList.AddListElement(SplitStringByStyles(currentLine[(2 + 4 * indentAmount)..]));
+                            lastList.AddListElement([markdownList]);
+                            continue;
+                        }
+                        else
+                        {
+                            list = existingList;
+                        }
                     }
                     else
                     {
                         list = new Elements.MarkdownList(false);
                         content.Add(list);
                     }
-                    string listItemContent = currentLine[2..]; // Remove the list marker and space
-                    list.AddListElement(SplitStringByStyles(listItemContent));
+                    if (list != null)
+                    {
+                        string listItemContent = currentLine[2..]; // Remove the list marker and space
+                        list.AddListElement(SplitStringByStyles(listItemContent));
+                    }
                     continue;
                 }
 
@@ -208,7 +252,7 @@ namespace Notatnik
         }
 
         [GeneratedRegex(@"^(?:\*{3,}|-{3,}|_{3,})$")] private static partial Regex RuleRegex();
-        [GeneratedRegex(@"^[-\*\+]\s")] private static partial Regex UnorderedListRegex();
+        [GeneratedRegex(@"^(?<indent>(?:\t| {4})*)[-\*\+]\s")] private static partial Regex UnorderedListRegex();
         [GeneratedRegex(@"^\d+\.\s")] private static partial Regex OrderedListRegex();
         [GeneratedRegex(@"^1\.\s")] private static partial Regex OrderedListFirstElementRegex();
     }
