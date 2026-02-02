d545648cb028c2192eaa20f6940bbbf7b21d1ade
monteferattomultiacc14 <monteferattomultiacc14@gmail.com>
2026-02-02 07:43:14 +0100

    Add files via upload

--- Notatnik/Notatnik/MainWindow.xaml.cs
+++ Notatnik/Notatnik/MainWindow.xaml.cs
@@ -21,6 +21,117 @@ namespace Notatnik
             InitializeComponent();
         }
 
+    
+    private void selectheader(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Visible;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+
+        }
+        private void selectrule(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Visible;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selectcodeblock(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Visible;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selectimage(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Visible;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selecttable(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Visible;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selecttasklist(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Visible;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selectTextBlock(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Visible;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selectListItem(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Visible;
+            framelist.Visibility = Visibility.Hidden;
+        }
+        private void selectlist(object sender, RoutedEventArgs e)
+        {
+            frameheader.Visibility = Visibility.Hidden;
+            framerule.Visibility = Visibility.Hidden;
+            framecodeblock.Visibility = Visibility.Hidden;
+            frameimage.Visibility = Visibility.Hidden;
+            frametable.Visibility = Visibility.Hidden;
+            frametasklist.Visibility = Visibility.Hidden;
+            frametextblock.Visibility = Visibility.Hidden;
+            framelistitem.Visibility = Visibility.Hidden;
+            framelist.Visibility = Visibility.Visible;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
 
