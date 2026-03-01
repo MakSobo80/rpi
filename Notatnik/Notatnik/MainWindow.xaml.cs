55afc4271ff4edfa0cd3c1ac592aba716ab54714
Maksymilian Sobolewski <maksobo80@edu.gdansk.pl>
2026-03-01 17:36:32 +0100

    merge

--- Notatnik/Notatnik/MainWindow.xaml.cs
+++ Notatnik/Notatnik/MainWindow.xaml.cs
@@ -1,3 +1,46 @@
+<<<<<<< HEAD
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Notatnik
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            var user = Session.LoggedInUser;
+            if (user != null)
+            {
+                loggedInAs.Content = $"Zalogowano jako: {user?.Login}\n z organizacji {Database.GetOrganization(user?.organizationId).Name}";
+            } else
+            {
+                loggedInAs.Content = "Nie zalogowano";
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+=======
 ﻿using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -26,7 +69,7 @@ namespace Notatnik
             {
                 notepad.WrittenText = TextContent.Text;
             };
-            var user = Session.LoggedInUser;
+            var user = SessionData.LoggedInUser;
             if (user != null)
             {
                 loggedInAs.Content = $"Zalogowano jako: {user?.Login}\n z organizacji {Database.GetOrganization(user?.organizationId).Name}";
