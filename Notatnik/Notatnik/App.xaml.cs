5ef0d3a2416efec965c4e6426582ef16ac2bb893
Maksymilian Sobolewski <maksobo80@edu.gdansk.pl>
2026-03-16 08:52:48 +0100

    Fixed potential NullReferenceException with fetching organization's name

--- Notatnik/Notatnik/App.xaml.cs
+++ Notatnik/Notatnik/App.xaml.cs
@@ -25,7 +25,11 @@ namespace Notatnik
                     var mainWindow = new MainWindow();
                     Application.Current.MainWindow = mainWindow;
                     if(Session.LoggedInUser != null)
-                        MessageBox.Show("Zalogowano jako: " + Session.LoggedInUser.Login + " z organizacji " + Database.GetOrganization(Session.LoggedInUser.organizationId).Name);
+                    {
+                        var org = Database.GetOrganization(Session.LoggedInUser.organizationId);
+                        var orgName = org?.Name ?? "brak";
+                        MessageBox.Show("Zalogowano jako: " + Session.LoggedInUser.Login + " z organizacji " + orgName);
+                    }
                     else
                         MessageBox.Show("Kontynuowano bez logowania");
                     mainWindow.Show();
